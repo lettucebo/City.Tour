2f752b15ba6d709d485a85da225ef93f92581f2e
Money <abc12207@gmail.com>
2018-02-09 16:15:59 +0800

    set appinsight

--- City.Tour.Web/App_Start/FilterConfig.cs
+++ City.Tour.Web/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace City.Tour.Web
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
