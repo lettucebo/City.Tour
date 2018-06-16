0009ea9f2d025da228f725531dc9693c71004647
Money <abc12207@gmail.com>
2018-06-17 01:34:32 +0800

    remove unnecessary file

--- City.Tour.Web/Controllers/HomeController.cs
+++ City.Tour.Web/Controllers/HomeController.cs
@@ -10,27 +10,9 @@ namespace City.Tour.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private TourService tourService = new TourService();
-
         public ActionResult Index()
         {
             return RedirectToAction("Login", "Account");
-        }
-
-        public ActionResult Story()
-        {
-            return View();
-        }
-
-        public ActionResult Map()
-        {
-            return View();
-        }
-
-        public ActionResult Tour(SortOrder sort = null, int page = 1)
-        {
-            var datas = tourService.ListAllToPagedAndFilterSort("", sort, page);
-            return View(datas);
         }
     }
 }
